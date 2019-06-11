c614c9d95aa2ca780ff4d846c6a221df078fbece
Ruslan <overko@windowslive.com>
2019-06-11 14:22:11 +0000

    Updates

---
+++ precache-manifest.c10b3a60b01feb42b8599b679fecc3ba.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21dbe0ac2f651a2264f2dcbee86178b0",
+    "url": "/market/index.html"
+  },
+  {
+    "revision": "0cc5ca079b91eed7281b",
+    "url": "/market/static/css/main.e85ce3cc.chunk.css"
+  },
+  {
+    "revision": "fe8ba6b805a932c1f96b",
+    "url": "/market/static/js/2.fb7e1e50.chunk.js"
+  },
+  {
+    "revision": "0cc5ca079b91eed7281b",
+    "url": "/market/static/js/main.7f204fa9.chunk.js"
+  },
+  {
+    "revision": "7f97cb6cd53287aa535a",
+    "url": "/market/static/js/runtime~main.9b3e2a4e.js"
+  },
+  {
+    "revision": "8e312aae0f7c2ca89fd42084bf3238ea",
+    "url": "/market/static/media/audio.8e312aae.jpg"
+  },
+  {
+    "revision": "f3b76159d13b0ba9d7b093b88e88afc2",
+    "url": "/market/static/media/laptop.f3b76159.jpg"
+  },
+  {
+    "revision": "05267ec063cef247ee0e09e987dde1cd",
+    "url": "/market/static/media/macbook.05267ec0.jpg"
+  },
+  {
+    "revision": "7ce66571a354ee40055f27bbeecc4404",
+    "url": "/market/static/media/nikon.7ce66571.jpg"
+  },
+  {
+    "revision": "62215f0da0060a345422ee5f31ed8f55",
+    "url": "/market/static/media/padAcer.62215f0d.jpg"
+  },
+  {
+    "revision": "787fa4099ae86e355df2440a8c0a8711",
+    "url": "/market/static/media/phone.787fa409.jpg"
+  },
+  {
+    "revision": "b695235aff1ca0532f0535207ab039b6",
+    "url": "/market/static/media/tvLg.b695235a.jpg"
+  },
+  {
+    "revision": "d046d8e280fbcfbff4fb8ed72de5611f",
+    "url": "/market/static/media/tvSamsung.d046d8e2.jpg"
+  }
+]);
